5705d4cc8542e74c880e3bcedbae80b5127e2669
Alexander <50303374+AlexanderXIV@users.noreply.github.com>
2020-05-07 15:40:34 +0200

    Add files via upload

--- magic_cube.nim
+++ magic_cube.nim
@@ -2,6 +2,7 @@ import os, math, ../utils, ../render3
 
 for i in 0..10:
   echo "\n"
+
 # 0: oben, weiß
 # 1: links, grün
 # 2: hinten, rot
@@ -9,18 +10,16 @@ for i in 0..10:
 # 4: vorne, orange
 # 5: unten, gelb
 
-let optimal_fps = 60.0
-
-var
-  s = 1.0
-  d = 0.1
-  sh = s / 2
-  sd = 1.5 * s + d
-
 type
   Cube = array[6, array[9, Color]]
 
 let
+  optimal_fps = 60.0
+
+  cube_max = 1.0
+  cube_min = cube_max / 2
+  module_distance = cube_max / 10
+
   window = new_window("Cube", resizable=true)
 
   black = rgb(0, 0, 0)
@@ -31,16 +30,19 @@ let
   orange = rgb(1, 0.5, 0)
   yellow = rgb(1, 1, 0)
 
-let face = Mesh()
+  face = Mesh()
 
 var
+  s = 0.0
+  d = module_distance
+
   cont = new_orbit_camera_controller()
   ren = new_render3(window)
   stats = Stats()
   is_running = true
 
-face.add_quad(Vec3(x: -sh, y: -sh, z: 0), Vec3(x: s), Vec3(y: s))
-face.add_quad(Vec3(x: -sh, y: -sh, z: 0), Vec3(y: s), Vec3(x: s))
+face.add_quad(Vec3(x: -0.5, y: -0.5, z: 0), Vec3(x: 1), Vec3(y: 1))
+face.add_quad(Vec3(x: -0.5, y: -0.5, z: 0), Vec3(y: 1), Vec3(x: 1))
 
 var cube: Cube
 for i in 0..<9:
@@ -51,17 +53,34 @@ for i in 0..<9:
   cube[4][i] = orange
   cube[5][i] = yellow
 
-proc draw_x(cube: Cube, ren1: var Render3, height: float, angle: float, scale: float) =
-  let
-    rd = degToRad(-angle)
-    x1 = cos(rd) * (s + d)
-    y1 = sin(rd) * (s + d)
+# proc draw_x(cube: Cube, ren1: var Render3, height: float, angle: float, scale: float) =
+#   let
+#     rd = degToRad(-angle)
+#     x1 = cos(rd) * (s + d)
+#     y1 = sin(rd) * (s + d)
 
-  for item in @[(0.0, 0.0), (x1, y1), (-x1, -y1), (y1, -x1), (-y1, x1), (x1 + y1, y1 - x1), (-x1 - y1, x1 - y1), (y1 - x1, -x1 - y1), (x1 - y1, x1 + y1)]:
-    ren1.add(face, color = cube[0][0], pos = Vec3(x: item[0], y: height + 0.5, z: item[1]), scale = scale, rot = new_quat(Vec3(y: 1), Deg(angle)) * new_quat(Vec3(x: 1), Deg(90)))
+#   for item in @[(0.0, 0.0), (x1, y1), (-x1, -y1), (y1, -x1), (-y1, x1), (x1 + y1, y1 - x1), (-x1 - y1, x1 - y1), (y1 - x1, -x1 - y1), (x1 - y1, x1 + y1)]:
+#     ren1.add(face, color = cube[0][0], pos = Vec3(x: item[0], y: height, z: item[1]), scale = scale, rot = new_quat(Vec3(y: 1), Deg(angle)) * new_quat(Vec3(x: 1), Deg(90)))
+
+# proc draw_y(cube: Cube, ren1: var Render3, height: float, angle: float, scale: float) =
+#   let
+#     rd = degToRad(-angle)
+#     x1 = cos(rd) * (s + d)
+#     y1 = sin(rd) * (s + d)
+
+#   for item in @[(0.0, 0.0), (x1, y1), (-x1, -y1), (y1, -x1), (-y1, x1), (x1 + y1, y1 - x1), (-x1 - y1, x1 - y1), (y1 - x1, -x1 - y1), (x1 - y1, x1 + y1)]:
+#     ren1.add(face, color = cube[0][0], pos = Vec3(x: height, y: item[0], z: item[1]), scale = scale, rot = new_quat(Vec3(x: 1), Deg(angle)) * new_quat(Vec3(y: 1), Deg(90)))
+
+# proc draw_z(cube: Cube, ren1: var Render3, height: float, angle: float, scale: float) =
+#   let
+#     rd = degToRad(-angle)
+#     x1 = cos(rd) * (s + d)
+#     y1 = sin(rd) * (s + d)
+
+#   for item in @[(0.0, 0.0), (x1, y1), (-x1, -y1), (y1, -x1), (-y1, x1), (x1 + y1, y1 - x1), (-x1 - y1, x1 - y1), (y1 - x1, -x1 - y1), (x1 - y1, x1 + y1)]:
+#     ren1.add(face, color = cube[0][0], pos = Vec3(x: item[1], y: item[0], z: height), scale = scale, rot = new_quat(Vec3(z: 1), Deg(angle)) * new_quat(Vec3(z: 1), Deg(90)))
 
 var button_down = false
-var scale = 1.0
 var change = true
 var angle = 0.0
 
@@ -83,17 +102,20 @@ while is_running:
         button_down = false
       else: discard#echo event
 
-  if ((button_down and s > 0.5) or (button_down == false and s < 1)) and stats.average_fps() > 10:
+  var ready = true
+  for item in stats.recent_fps:
+    if item == 0.0:
+      ready = false
+      break;
+
+  if ((button_down and s > cube_min) or (button_down == false and s < cube_max)) and ready:
     change = true
-    if button_down and s > 0.5:
-      scale = scale - 1 / stats.average_fps()
+    if button_down and s > cube_min:
+      s = s - 1 / stats.average_fps()
     else:
-      scale = scale + 1 / stats.average_fps()
-      if scale > 1: scale = 1
-    s = scale
-    d = 1.6 - scale * 1.5
-    sh = s / 2
-    sd = 1.5 * s + d
+      s = s + 1 / stats.average_fps()
+      if s > cube_max: s = cube_max
+    d = 1.5 * (cube_max - s) + module_distance
 
   # ren.add(face, color = red)
   # ren.add(face, color = green, rot = new_quat(Vec3(y: 1), Deg(90)))
@@ -105,25 +127,21 @@ while is_running:
     ren.background(grey(1))
 
     for i1 in 0..<3:
-      var z = (i1.float - 1) * (s + d)
+      var z1 = (i1.float - 1) * (s + d)
       for i2 in 0..<3:
         var z2 = (i2.float - 1) * (s + d)
 
-        for val in @[-(sh + d), -sh, sh, sh + d]:
-          ren.add(face, color = black, pos = Vec3(x: z2, y: z, z: val), scale = scale)
-          ren.add(face, color = black, pos = Vec3(x: val, y: z2, z: z), scale = scale, rot = new_quat(Vec3(y: 1), Deg(90)))
-          ren.add(face, color = black, pos = Vec3(x: z, y: val, z: z2), scale = scale, rot = new_quat(Vec3(x: 1), Deg(90)))
+        for val in @[-(s / 2 + d), -s / 2, s / 2, s / 2 + d]:
+          ren.add(face, color = black, pos = Vec3(x: z2, y: z1, z: val), scale = s)
+          ren.add(face, color = black, pos = Vec3(x: val, y: z2, z: z1), scale = s, rot = new_quat(Vec3(y: 1), Deg(90)))
+          ren.add(face, color = black, pos = Vec3(x: z1, y: val, z: z2), scale = s, rot = new_quat(Vec3(x: 1), Deg(90)))
 
-        ren.add(face, color = cube[0][8 - (3 * i2 + i1)], pos = Vec3(x: z, y: sd, z: z2), scale = scale, rot = new_quat(Vec3(x: 1), Deg(90)))
-        ren.add(face, color = cube[1][8 - (3 * i1 + i2)], pos = Vec3(x: sd, y: z2, z: z), scale = scale, rot = new_quat(Vec3(y: 1), Deg(90)))
-        ren.add(face, color = cube[2][8 - (3 * i1 + (2 - i2))], pos = Vec3(x: z2, y: z, z: -sd), scale = scale)
-        ren.add(face, color = cube[3][8 - (3 * i2 + (2 - i1))], pos = Vec3(x: -sd, y: z2, z: z), scale = scale, rot = new_quat(Vec3(y: 1), Deg(90)))
-        ren.add(face, color = cube[4][3 * i2 + i1], pos = Vec3(x: z2, y: z, z: sd), scale = scale)
-        ren.add(face, color = cube[5][3 * (2 - i2) + i1], pos = Vec3(x: z, y: -sd, z: z2), scale = scale, rot = new_quat(Vec3(x: 1), Deg(90)))
-
-        angle = angle + 0.1
-        draw_x(cube, ren, sd, angle, scale)
-
+        ren.add(face, color = cube[0][8 - (3 * i2 + i1)], pos = Vec3(x: z1, y: (1.5 * s + d), z: z2), scale = s, rot = new_quat(Vec3(x: 1), Deg(90)))
+        ren.add(face, color = cube[1][8 - (3 * i1 + i2)], pos = Vec3(x: (1.5 * s + d), y: z2, z: z1), scale = s, rot = new_quat(Vec3(y: 1), Deg(90)))
+        ren.add(face, color = cube[2][8 - (3 * i1 + (2 - i2))], pos = Vec3(x: z2, y: z1, z: -(1.5 * s + d)), scale = s)
+        ren.add(face, color = cube[3][8 - (3 * i2 + (2 - i1))], pos = Vec3(x: -(1.5 * s + d), y: z2, z: z1), scale = s, rot = new_quat(Vec3(y: 1), Deg(90)))
+        ren.add(face, color = cube[4][3 * i2 + i1], pos = Vec3(x: z2, y: z1, z: (1.5 * s + d)), scale = s)
+        ren.add(face, color = cube[5][3 * (2 - i2) + i1], pos = Vec3(x: z1, y: -(1.5 * s + d), z: z2), scale = s, rot = new_quat(Vec3(x: 1), Deg(90)))
 
     ren.add(Light(kind: LightAmbient,
       ambient: 1
@@ -135,7 +153,7 @@ while is_running:
     stdout.write("\rfps: " & $stats.average_fps().int & "\t")
 
   if change == false:
-    sleep(10)
+    sleep(1000 div optimal_fps.int)
   elif change and stats.average_fps() > optimal_fps:
     let fps = stats.average_fps() 
     sleep((1000 * (fps - optimal_fps) / (optimal_fps * fps)).int)
